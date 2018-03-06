2af5ac9df50f391ebc1b007c3844aebffaf2697a
Jake <polyfish42>
2018-03-06 15:59:46 -0500

    Add initial styling for splash page before lecture

--- frontend/components/app/app.jsx
+++ frontend/components/app/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignupSessionFormContainer from '../session/signup_session_form_container';
 import LoginSessionFormContainer from '../session/login_session_form_container';
+import Splash from '../frontend/splash';
 import { AuthRoute } from '../../util/route_util';
 
 const App = () => {
   return (
     <div>
+      <AuthRoute path='/' exact component={Splash} />
       <AuthRoute path='/signup' component={SignupSessionFormContainer} />
       <AuthRoute path='/login' component={LoginSessionFormContainer} />
     </div>
