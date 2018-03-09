9f4a85ad92971ec2cc1825c382b2f7ce48d84763
Jake <polyfish42@gmail.com>
2018-03-08 22:32:05 -0500

    Recreate app page (copy pasted) to fool Heroku and git into updating

--- frontend/components/app/app.jsx
+++ frontend/components/app/app.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import ProjectHeaderContainer from '../project/project_header/project_header_container';
-import SplashContainer from '../splash/splash_container';
-import SignupSessionFormContainer from '../session/signup_session_form_container';
-import LoginSessionFormContainer from '../session/login_session_form_container';
-import Project from '../project/project';
-import MessageBoardContainer from '../message_board/message_board_container';
-import TodoListIndexContainer from '../todos/todo_list/todo_list_index_container';
-import TodoListShowContainer from '../todos/todo_list/todo_list_show_container';
-import ScheduleContainer from '../schedule/schedule_container';
-import { AuthRoute, ProtectedRoute } from '../../util/route_util';
+import React from "react";
+import ProjectHeaderContainer from "../project/project_header/project_header_container";
+import SplashContainer from "../splash/splash_container";
+import SignupSessionFormContainer from "../session/signup_session_form_container";
+import LoginSessionFormContainer from "../session/login_session_form_container";
+import Project from "../project/project";
+import MessageBoardContainer from "../message_board/message_board_container";
+import TodoListIndexContainer from "../todos/todo_list/todo_list_index_container";
+import TodoListShowContainer from "../todos/todo_list/todo_list_show_container";
+import ScheduleContainer from "../schedule/schedule_container";
+import { AuthRoute, ProtectedRoute } from "../../util/route_util";
 
 const App = () => {
   return (
     <div>
       <ProjectHeaderContainer />
-      <AuthRoute path='/' exact component={SplashContainer} />
-      <AuthRoute path='/signup' component={SignupSessionFormContainer} />
-      <AuthRoute path='/login' component={LoginSessionFormContainer} />
-      <ProtectedRoute path='/project' component={Project} />
-      <ProtectedRoute path='/messages' component={MessageBoardContainer} />
-      <ProtectedRoute path='/todolists' exact component={TodoListIndexContainer} />
-      <ProtectedRoute path='/todolists/:todoListId' component={TodoListShowContainer} />
-      <ProtectedRoute path='/schedule' component={ScheduleContainer} />
+      <AuthRoute path="/" exact component={SplashContainer} />
+      <AuthRoute path="/signup" component={SignupSessionFormContainer} />
+      <AuthRoute path="/login" component={LoginSessionFormContainer} />
+      <ProtectedRoute path="/project" component={Project} />
+      <ProtectedRoute path="/messages" component={MessageBoardContainer} />
+      <ProtectedRoute
+        path="/todolists"
+        exact
+        component={TodoListIndexContainer}
+      />
+      <ProtectedRoute
+        path="/todolists/:todoListId"
+        component={TodoListShowContainer}
+      />
+      <ProtectedRoute path="/schedule" component={ScheduleContainer} />
     </div>
   );
 };
