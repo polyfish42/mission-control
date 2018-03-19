7ec98852cb71c9ac7ab49090891115a483f6b63a
Jacob Brady <polyfish42@gmail.com>
2018-03-19 13:59:56 -0400

    Add bottom border to top menu

--- frontend/components/project/project_header/project_header.jsx
+++ frontend/components/project/project_header/project_header.jsx
@@ -6,11 +6,25 @@ import { Link } from "react-router-dom";
 class ProjectHeader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { logout_panel_showing: false };
+    this.state = { logout_panel_showing: false, scrolling: false };
 
+    this.handleScroll = this.handleScroll.bind(this);
     this.togglePanel = this.togglePanel.bind(this);
     this.logout = this.logout.bind(this);
     this.resetParentState = this.resetParentState.bind(this);
+  }
+
+  handleScroll() {
+    const scrolling = window.scrollY > 1 ? true : false
+    this.setState({ scrolling })
+  }
+
+  componentWillMount() {
+    document.addEventListener("scroll", this.handleScroll, false)
+  }
+
+  componentWillUnmount() {
+    document.addEventListener("scroll", this.handleScroll, false)
   }
 
   render() {
@@ -19,24 +33,32 @@ class ProjectHeader extends React.Component {
     }
 
     return (
-      <div className="project-header">
-        <LoadingBar progressIncrease={35} className="loading-bar"/>
-        <nav className="project-header__nav">
-          <Link to="/project">
-            <img src={window.smallLogo} className="project-header__img" />
-          </Link>
-          <img
-            src={window.avatar}
-            className="project-header__img"
-            onClick={this.togglePanel}
-          />
-        </nav>
-        {this.state.logout_panel_showing && (
-          <DropDownMenu
-            resetParentState={this.resetParentState}
-            logout={this.logout}
-          />
-        )}
+      <div>
+        <div className="project-header">
+          <LoadingBar
+            progressIncrease={35}
+            className="loading-bar"/>
+          <nav className="project-header__nav">
+            <Link to="/project">
+              <img
+                src={window.smallLogo}
+                className="project-header__img" />
+            </Link>
+            <img
+              src={window.avatar}
+              className="project-header__img"
+              onClick={this.togglePanel}
+              />
+          </nav>
+          {this.state.logout_panel_showing && (
+            <DropDownMenu
+              resetParentState={this.resetParentState}
+              logout={this.logout}
+              />
+          )}
+          <div className={`project-header__scroll ${this.state.scrolling ? "project-header__scroll--scrolling" : ""}`}>
+        </div>
+        </div>
       </div>
     );
   }
