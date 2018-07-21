331d6498efb39ae8a8b7b7834aed65b99753d100
Jacob Brady <polyfish42@gmail.com>
2018-07-21 12:32:39 -0400

    Cleanup project code formatting

--- frontend/components/project/project.jsx
+++ frontend/components/project/project.jsx
@@ -6,24 +6,24 @@ import { Link } from 'react-router-dom';
 
 class Project extends React.Component {
   componentWillMount() {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
   }
-  
-  render () {
+
+  render() {
     return (
       <div className="main-content">
         <h1 className="main-content__h1">Mission Control</h1>
-          <div className="project">
-            <div className="project__tools">
-              <Link to="/messages" className="project__link">
-                <MessageBoardToolContainer />
-              </Link>
-              <Link to="/todolists" className="project__link">
-                <TodosToolContainer />
-              </Link>
-              <Link to="/events" className="project__link">
-                <ScheduleToolContainer />
-              </Link>
+        <div className="project">
+          <div className="project__tools">
+            <Link to="/messages" className="project__link">
+              <MessageBoardToolContainer />
+            </Link>
+            <Link to="/todolists" className="project__link">
+              <TodosToolContainer />
+            </Link>
+            <Link to="/events" className="project__link">
+              <ScheduleToolContainer />
+            </Link>
           </div>
         </div>
       </div>
